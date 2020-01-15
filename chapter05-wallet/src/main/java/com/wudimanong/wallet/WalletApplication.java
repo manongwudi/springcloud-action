bc55e7b0a6f6f0d239c06e811e02ce4a03a45fce
jiangqiao <jiangqiao@mafengwo.com>
2020-01-15 10:54:49 +0800

    Spring Cloud微服务互联网项目实战-第5章源码 电子账户开户逻辑

--- chapter05-wallet/src/main/java/com/wudimanong/wallet/WalletApplication.java
+++ chapter05-wallet/src/main/java/com/wudimanong/wallet/WalletApplication.java
@@ -1,5 +1,6 @@
 package com.wudimanong.wallet;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+@MapperScan("com.wudimanong.wallet.dao.mapper")
 public class WalletApplication {
 
     public static void main(String[] args) {
