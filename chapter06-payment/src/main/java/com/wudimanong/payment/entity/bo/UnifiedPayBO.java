d03c37e4091015d663137ff1428fa639f0f50e9a
jiangqiao <jiangqiao@mafengwo.com>
2020-02-12 16:45:43 +0800

    Spring Cloud微服务互联网项目实战-第6章源码-新增统一支付接口定义

---
+++ chapter06-payment/src/main/java/com/wudimanong/payment/entity/bo/UnifiedPayBO.java
@@ -0,0 +1,46 @@
+package com.wudimanong.payment.entity.bo;
+
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * @author jiangqiao
+ */
+@Data
+public class UnifiedPayBO implements Serializable {
+
+    /**
+     * 商户支付订单号
+     */
+    private Long orderId;
+
+    /**
+     * 第三方支付渠道生成预支付订单号
+     */
+    private String tradeNo;
+
+    /**
+     * 支付订单金额
+     */
+    private Integer amount;
+
+    /**
+     * 支付币种
+     */
+    private String currency;
+
+    /**
+     * 支付渠道编码
+     */
+    private String channel;
+
+    /**
+     * 特殊支付场景所需传递的额外支付信息
+     */
+    private String extraInfo;
+
+    /**
+     * 支付订单状态，0-待支付；1-支付中；2-支付成功；3-支付失败
+     */
+    private Integer payStatus;
+}
---
+++ chapter06-payment/src/main/java/com/wudimanong/payment/service/impl/PayServiceImpl.java
@@ -0,0 +1,20 @@
+package com.wudimanong.payment.service.impl;
+
+import com.wudimanong.payment.entity.bo.UnifiedPayBO;
+import com.wudimanong.payment.entity.dto.UnifiedPayDTO;
+import com.wudimanong.payment.service.PayService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author jiangqiao
+ */
+@Slf4j
+@Service
+public class PayServiceImpl implements PayService {
+
+    @Override
+    public UnifiedPayBO unifiedPay(UnifiedPayDTO unifiedPayDTO) {
+        return null;
+    }
+}
