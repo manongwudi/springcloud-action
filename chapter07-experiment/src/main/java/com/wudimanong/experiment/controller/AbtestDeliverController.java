005bc7789d9f017487562ea1fe2e17df8e605fa8
jiangqiao <jiangqiao@mafengwo.com>
2020-03-17 01:06:02 +0800

    Spring Cloud微服务互联网项目实战-第7章源码更新->统一接口+SDK工程拆分

--- chapter07-experiment/src/main/java/com/wudimanong/experiment/controller/AbtestDeliverController.java
+++ chapter07-experiment/src/main/java/com/wudimanong/experiment/controller/AbtestDeliverController.java
@@ -1,5 +1,6 @@
 package com.wudimanong.experiment.controller;
 
+import com.wudimanong.experiment.client.entity.ResponseResult;
 import com.wudimanong.experiment.dao.model.AbtestExpInfoPO;
 import com.wudimanong.experiment.service.AbtestExpInfoService;
 import lombok.extern.slf4j.Slf4j;
@@ -30,28 +31,8 @@ public class AbtestDeliverController {
      * @param factorTag
      */
     @GetMapping("/findByFactorTag")
-    public AbtestExpInfoPO findByFactorTag(@RequestParam("factorTag") String factorTag) throws InterruptedException {
-        Long startTime1 = System.currentTimeMillis();
+    public ResponseResult<AbtestExpInfoPO> findByFactorTag(@RequestParam("factorTag") String factorTag) {
         AbtestExpInfoPO abtestExpInfoPO = abtestExpInfoServiceImpl.getExpInfoByFactorTag(factorTag);
-        long endTime1 = System.currentTimeMillis();
-        System.out.println("第一次耗时（数据库获取）->" + (endTime1 - startTime1) + " 毫秒");
-
-        Long startTime2 = System.currentTimeMillis();
-        AbtestExpInfoPO abtestExpInfoPO2 = abtestExpInfoServiceImpl.getExpInfoByFactorTag(factorTag);
-        long endTime2 = System.currentTimeMillis();
-        System.out.println("第二次耗时（从缓存获取）->" + (endTime2 - startTime2) + " 毫秒");
-
-        Thread.sleep(5000);
-
-        Long startTime3 = System.currentTimeMillis();
-        AbtestExpInfoPO abtestExpInfoPO3 = abtestExpInfoServiceImpl.getExpInfoByFactorTag(factorTag);
-        long endTime3 = System.currentTimeMillis();
-        System.out.println("第三次次耗时（从数据库获取）->" + (endTime3 - startTime3) + " 毫秒");
-
-        Long startTime4 = System.currentTimeMillis();
-        AbtestExpInfoPO abtestExpInfoPO4 = abtestExpInfoServiceImpl.getExpInfoByFactorTag(factorTag);
-        long endTime4 = System.currentTimeMillis();
-        System.out.println("第四次次耗时（从缓存获取）->" + (endTime4 - startTime4) + " 毫秒");
-        return abtestExpInfoPO4;
+        return ResponseResult.OK(abtestExpInfoPO);
     }
 }
