11127f7d1712bcadb8be89a2e7c0373e5db0b4f8
jiangqiao <jiangqiao@mafengwo.com>
2021-01-20 09:57:26 +0800

    第10章，skywalking部署实验fix

--- chapter10-monitor-demo/src/main/java/com/wudimanong/monitor/controller/MonitorController.java
+++ chapter10-monitor-demo/src/main/java/com/wudimanong/monitor/controller/MonitorController.java
@@ -1,7 +1,10 @@
 package com.wudimanong.monitor.controller;
 
+import com.wudimanong.monitor.service.MonitorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,8 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/monitor")
 public class MonitorController {
 
+    @Autowired
+    private MonitorService monitorServiceImpl;
+
     @GetMapping("/test")
-    public String monitorTest() {
+    public String monitorTest(@RequestParam("name") String name) {
+        monitorServiceImpl.monitorTest(name);
         return "监控示范工程测试接口返回->OK!";
     }
 }
---
+++ chapter10-monitor-demo/src/main/java/com/wudimanong/monitor/service/MonitorService.java
@@ -0,0 +1,9 @@
+package com.wudimanong.monitor.service;
+
+/**
+ * @author jiangqiao
+ */
+public interface MonitorService {
+
+    String monitorTest(String name);
+}
